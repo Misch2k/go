a082bdfd2a6de2e4e85a381116bfe606c3c20d13
Michel Kugler <michel.k@gmx.ch>
2017-03-02 19:52:26 +0100

    Learn Part from Golang.org

---
+++ src/go_tour/type-inference.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	v := 44 // change me!
+	w := "44" // change me!
+	x := 44.5 // change me!
+	fmt.Printf("v is of type %T\n", v)
+	fmt.Println(v)
+	fmt.Printf("w is of type %T\n", w)
+	fmt.Println(w)
+	fmt.Printf("x is of type %T\n", x)
+	fmt.Println(x)
+}
+
